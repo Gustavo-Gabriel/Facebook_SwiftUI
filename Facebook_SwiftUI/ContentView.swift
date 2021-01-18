24928a4e84708026af1952b2e9915a1c4363bda0
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-18 16:55:20 -0400

    fix: refatorando o código

--- Facebook_SwiftUI/ContentView.swift
+++ Facebook_SwiftUI/ContentView.swift
@@ -7,11 +7,32 @@
 
 import SwiftUI
 
+struct FBPostModel: Hashable {
+    let name: String
+    let post: String
+    let imageName: String
+}
+
 struct ContentView: View {
     
     @Binding var text: String
     
     let stories = ["story1", "story2", "story3", "story1", "story2", "story3"]
+    
+    let posts: [FBPostModel] = [
+        
+        FBPostModel(name: "Mark Zuckerberg",
+                    post: "Hey guys I made this cool website called thefacebook to see if Im cool or not",
+                    imageName: "person1"),
+        
+        FBPostModel(name: "Jeff Bezos",
+                    post: "You'll see once I take over the world with Amazon",
+                    imageName: "person2"),
+        
+        FBPostModel(name: "Bill Gates",
+                    post: "Who cares - I made Windows!",
+                    imageName: "person3")
+    ]
     
     // Cor do Texto do Facebook
     let facebookBlue = UIColor(red: 23/255.0, green: 120/255.0, blue: 242/255.0, alpha: 1)
@@ -49,123 +70,19 @@ struct ContentView: View {
                 ScrollView(.vertical){
                     VStack{
                         
-                        ScrollView(.horizontal, showsIndicators: false){
-                            HStack(spacing: 3){
-                                ForEach(stories, id: \.self) { name in
-                                    Image(name)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: 140, height: 200, alignment: .center)
-                                        .background(Color.red)
-                                        .clipped()
-                                }
-                            }
+                        StoriesView(stories: stories)
+                        
+                        ForEach(posts, id: \.self) { model in
+                            FBPost(model: model)
+                            Spacer()
                         }
-                        .padding()
-                        
-                        FBPost(name: "Mark Zuckerberg",
-                               post: "Hey guys I made this cool website called thefacebook to see if Im cool or not",
-                               imageName: "person1")
-                        Spacer()
-                        
-                        FBPost(name: "Jeff Bezos",
-                               post: "You'll see once I take over the world with Amazon",
-                               imageName: "person2")
-                        Spacer()
-                        
-                        FBPost(name: "Bill Gates",
-                               post: "Who cares - I made Windows!",
-                               imageName: "person3")
-                        Spacer()
                         
                     }
                 }
             }
-                
             
             Spacer()
         }
-        
-    }
-}
-
-struct FBPost: View{
-    
-    @State var isLiked: Bool = false
-    
-    let name: String
-    let post: String
-    let imageName: String
-    
-    var body: some View {
-    
-        VStack{
-            HStack{
-                Image(imageName)
-                    .resizable()
-                    .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                    .cornerRadius(25)
-                
-                VStack{
-                    
-                    HStack{
-                        Text(name)
-                            .foregroundColor(Color.blue)
-                            .font(.system(size: 18, weight: .semibold, design: .default))
-                        Spacer()
-                    }
-                    
-                    HStack{
-                        Text("12 minutes ago")
-                            .foregroundColor(Color(.secondaryLabel))
-                        Spacer()
-                    }
-                    
-                }
-                
-                Spacer()
-            }
-            
-            Spacer()
-            
-            HStack{
-                Text(post)
-                    .font(.system(size: 24, weight: .regular, design: .default))
-                    .multilineTextAlignment(.leading)
-                Spacer()
-            }
-            
-            Spacer()
-            
-            HStack{
-                Button(action: {
-                    isLiked.toggle()
-                }, label: {
-                    Text(isLiked ? "Liked" : "Like")
-                })
-                
-                Spacer()
-                
-                Button(action: {
-                    
-                }, label: {
-                    Text("Comment")
-                })
-                
-                Spacer()
-                
-                Button(action: {
-                    
-                }, label: {
-                    Text("Share")
-                })
-            }
-            .padding()
-        }
-        .padding()
-        .background(Color(.systemBackground))
-        .cornerRadius(7)
         
     }
 }
---
+++ Facebook_SwiftUI/StoriesView.swift
@@ -0,0 +1,32 @@
+//
+//  StoriesView.swift
+//  Facebook_SwiftUI
+//
+//  Created by Gustavo Anjos on 18/01/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct StoriesView: View {
+    
+    let stories: [String]
+    
+    var body: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack(spacing: 3){
+                ForEach(stories, id: \.self) { name in
+                    Image(name)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 140, height: 200, alignment: .center)
+                        .background(Color.red)
+                        .clipped()
+                }
+            }
+        }
+        .padding()
+        
+    }
+}
