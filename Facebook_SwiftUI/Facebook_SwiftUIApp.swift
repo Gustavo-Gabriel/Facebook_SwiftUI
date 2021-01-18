9f4b326cabd27b47bfbe15dbdaf1b3a563ed846b
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-18 11:27:08 -0400

    Initial Commit

---
+++ Facebook_SwiftUI/Facebook_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facebook_SwiftUIApp.swift
+//  Facebook_SwiftUI
+//
+//  Created by Gustavo Anjos on 18/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Facebook_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
