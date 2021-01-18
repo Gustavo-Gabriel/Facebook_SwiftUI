b2a313bfb69348d8a2fdd20721fb2738adbf569c
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-18 11:49:11 -0400

    feat: inserir o header da interface com texto e imagem

--- Facebook_SwiftUI/ContentView.swift
+++ Facebook_SwiftUI/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Cor do Texto do Facebook
+    let facebookBlue = UIColor(red: 23/255.0, green: 120/255.0, blue: 242/255.0, alpha: 1)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        // A Pilha no Swift UI pode ser utilizada tanto na Vertical, Horizontal e no Eixo Z
+        VStack{
+            // Alinhamento
+            HStack{
+                Text("facebook") // Propriedades que modificam o Text
+                    .font(.system(size: 48, weight: .bold, design: .default))
+                    .foregroundColor(Color(facebookBlue))
+                
+                // Separar Views
+                Spacer()
+                
+                Image(systemName: "person.circle")
+                    // Preencher todo o espacao da View
+                    .resizable()
+                    .frame(width: 45, height: 45, alignment: .center)
+                    .foregroundColor(.secondary)
+            }
+            .padding() // Espaco nas Margens
+            
+            Spacer()
+        }
+        
     }
 }
 
